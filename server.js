32ca59da05c60a99ae5f74adac090842a8990aed
includewasim <wasimkhanzabi222@gmail.com>
2024-07-24 09:29:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ app.use('/api/v1/transactions', require('./routes/transactionRoutes'));
 
 
 //static files
-app.use(express.static[path.join(__dirname,'./client/build')])
+app.use(express.static(path.join(__dirname,'./client/build')))
+
 app.get('*', function (req,res) {
     res.sendFile(path.join(__dirname,"./client/build/index.html"));
 })
